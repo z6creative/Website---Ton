c5baeebf1d5910b09957993ad132c23ed1492220
Shadayy <shadayy@gmail.com>
2015-03-01 14:38:07 -0300

    Correção dos videos
    Videos agora estão centralizados.
    Botões de controle abaixo do video foram removidos.
    Botão de próximo e anterior não fazem a página rolar

--- js/custom.js
+++ js/custom.js
@@ -32,10 +32,12 @@
 
 		$('a[href*=#]').bind("click", function(e){
 			var anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top
-			}, 1000);
-			e.preventDefault();
+			if(anchor.context.hash != '#carouselVideos'){
+				$('html, body').stop().animate({
+					scrollTop: $(anchor.attr('href')).offset().top
+				}, 1000);
+				e.preventDefault();
+			}
 		});
 
 		/* ---------------------------------------------- /*
